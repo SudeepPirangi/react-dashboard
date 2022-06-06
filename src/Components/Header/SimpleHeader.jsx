2f7b783125288b79dfee640e36bcabd547e869fa
SudeepPirangi <sudeep_edu@yahoo.com>
2022-06-06 19:55:19 +0530

    Adds finished header

--- src/Components/Header/SimpleHeader.jsx
+++ src/Components/Header/SimpleHeader.jsx
@@ -1,17 +1,25 @@
 import React from "react";
-import { Navbar, Nav } from "rsuite";
+import { Navbar, Nav, Avatar } from "rsuite";
+import { FaBell } from "react-icons/fa";
+
+import SearchBox from "../CommonUI/SearchBox";
 
 const SimpleNavbar = () => {
   return (
     <Navbar>
       <Navbar.Brand href="#">CORKOWL</Navbar.Brand>
       <Nav>
-        <Nav.Item>Search box</Nav.Item>
+        <Nav.Item>
+          <SearchBox width={300} />
+        </Nav.Item>
       </Nav>
       <Nav pullRight>
-        <Nav.Menu title="Sudeep Pirangi">
-          <Nav.Item>Company</Nav.Item>
-          <Nav.Item>Team</Nav.Item>
+        <Nav.Item>
+          <FaBell style={{ fontSize: "1.5em" }} />
+        </Nav.Item>
+        <Nav.Menu title={<NamedAvatar />} placement="bottomEnd">
+          <Nav.Item>View Profile</Nav.Item>
+          <Nav.Item>Logout</Nav.Item>
         </Nav.Menu>
       </Nav>
     </Navbar>
@@ -19,3 +27,11 @@ const SimpleNavbar = () => {
 };
 
 export default SimpleNavbar;
+
+const NamedAvatar = () => {
+  return (
+    <span className="row-flex" styles={{ alignItems: "center" }}>
+      Sudeep Pirangi &nbsp; <Avatar size="sm" circle src="https://avatars.githubusercontent.com/u/2797600" alt="@posebear1990" />
+    </span>
+  );
+};
