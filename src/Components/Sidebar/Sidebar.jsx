7224ab570b5b1d396cc8db94308b7ba1b28e2df8
SudeepPirangi <sudeep_edu@yahoo.com>
2022-06-06 20:16:11 +0530

    Adds copyright disclaimer

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -30,9 +30,31 @@ function Sidebar() {
             </Nav.Item>
           </Nav>
         </Sidenav.Body>
-        <Sidenav.Toggle expanded={expanded} onToggle={(expanded) => setExpanded(expanded)} />
+        <Copyright expanded={expanded} />
+        <Sidenav.Toggle expanded={expanded} onToggle={(expanded) => setExpanded(expanded)}></Sidenav.Toggle>
       </Sidenav>
     </div>
   );
 }
 export default Sidebar;
+
+const Copyright = ({ expanded }) => {
+  const styles = {
+    copyright: {
+      position: "absolute",
+      bottom: 0,
+      padding: "0 10px",
+      zIndex: 1,
+    },
+    h3: { fontSize: "0.9rem", lineHeight: "inherit" },
+    span: { fontSize: "0.7rem" },
+  };
+  return (
+    expanded && (
+      <div style={styles.copyright}>
+        <h3 style={styles.h3}>CorkOwl</h3>
+        <span style={styles.span}>&copy;2020 CorkOwl. All Rights Reserved.</span>
+      </div>
+    )
+  );
+};
